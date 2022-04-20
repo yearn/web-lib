a13ec6c1998692274a0a57b49963c540de0fdfe9
Majorfi <major-tom.eth@pm.me>
2022-04-20 13:04:24 +0200

    feat: banner update for Amsterdam

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import	React, {ReactElement}		from	'react';
-import	{Card, AlertBanner}			from	'@yearn/web-lib/components';
+import	{Card, Button}				from	'@yearn/web-lib/components';
 
 function	ColorBox({color, name}: {color: string, name: string}): ReactElement {
 	return (
@@ -97,16 +97,29 @@ function	Index(): ReactElement {
 	return (
 		<section aria-label={'some default section'}>
 			<div className={'mb-4'}>
-				<AlertBanner
-					id={'color-system'}
-					title={'Color system'}
-					canClose={false}
-					maxHeight={'max-h-[600px] md:max-h-[300px]'}>
-					<div>
-						<p className={'text-primary'}>{'With this color system, we are trying to mimic some material standard conventions, with the use of `primary`, `secondary`, `variant`, `background`, `surface`, etc. Naming if far from perfect yet, but it\'s a Work In Progress'}</p>
-						<p className={'block mt-4 text-primary'}>{'The colors are set using css variables and can be overrited in your style.css file.'}</p>
+				<div
+					className={'transition-max-height duration-600'}>
+					<div className={'grid overflow-hidden relative grid-cols-2 p-0 rounded-lg border-2 alertBanner--wrapper bg-secondary border-primary'}>
+						<div className={'p-6 mr-2'}>
+							<h4 className={'mb-6 text-typo-primary-variant'}>{'Take the blue pill, and follow the bunny.'}</h4>
+							<p className={'text-typo-primary-variant'}>{'At Eth Amsterdam, the most mind expanding drug you can take is the blue pill.'}</p>
+							<p className={'block mt-4 text-typo-primary-variant'}>{'Find the bunny:'}</p>
+							<div className={'mt-5'}>
+								<Button as={'a'} href={'https://twitter.com/iearnfinance'} className={'w-48'}>
+									{'Follow @iearnfinance'}
+								</Button>
+							</div>
+						</div>
+						<div
+							className={'overflow-hidden relative -my-1 ml-1 w-full h-full rounded-xl border-y-2 border-l-2 border-primary'}
+							style={{height: 'calc(100% + 6px)'}}>
+							<div
+								className={'h-[248px] rounded-lg image-align-middle'}>
+								<img src={'/mommy-bunny.jpg'} height={248} width={512} loading={'eager'} />
+							</div>
+						</div>
 					</div>
-				</AlertBanner>
+				</div>
 			</div>
 			<Card>
 				<ColorPaletteLight />
