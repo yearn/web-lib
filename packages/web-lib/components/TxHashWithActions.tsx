7914912f3e1c92b9c7f53039eb13aa409e43f13b
Majorfi <major-tom.eth@pm.me>
2022-06-05 10:56:47 +0200

    feat: expand elementWithActions classes

--- packages/web-lib/components/TxHashWithActions.tsx
+++ packages/web-lib/components/TxHashWithActions.tsx
@@ -1,33 +1,36 @@
-import	React, {ReactElement}			from	'react';
-import	{truncateHex, copyToClipboard}	from	'../utils/utils';
-import	IconLinkOut						from	'../icons/IconLinkOut';
-import	IconCopy						from	'../icons/IconCopy';
-import type * as TxHashWithActionsTypes	from 	'./TxHashWithActions.d';
+import	React, {ReactElement}				from	'react';
+import	{truncateHex, copyToClipboard}		from	'../utils/utils';
+import	IconLinkOut							from	'../icons/IconLinkOut';
+import	IconCopy							from	'../icons/IconCopy';
+import type * as TxHashWithActionsTypes		from	'./TxHashWithActions.d';
 
 function	TxHashWithActions({
 	txHash,
 	explorer = 'https://etherscan.io',
 	truncate = 5,
 	wrapperClassName,
-	className = 'font-mono font-bold text-left text-typo-primary'
+	className = ''
 }: TxHashWithActionsTypes.TTxHashWithActions): ReactElement {
 	return (
 		<span className={`flex flex-row items-center ${wrapperClassName}`}>
-			<p className={className}>{truncateHex(txHash, truncate)}</p>
-			<div className={'mx-2 md:mx-2'}>
-				<button
-					onClick={(): void => copyToClipboard(txHash)}
-					className={'flex justify-center items-center p-0 w-8 h-8 rounded-lg cursor-copy'}>
-					<IconCopy className={'w-4 h-4 transition-colors text-primary hover:text-primary-variant'} />
-				</button>
-			</div>
-			<button className={'flex justify-center items-center p-0 w-8 h-8 rounded-lg cursor-copy'}>
+			<p className={`yearn--elementWithActions ${className}`}>{truncateHex(txHash, truncate)}</p>
+			<button
+				className={'yearn--elementWithActions-copy'}
+				onClick={(e): void => {
+					e.stopPropagation();
+					copyToClipboard(txHash);
+				}}>
+				<IconCopy className={'yearn--elementWithActions-icon'} />
+			</button>
+			<button className={'yearn--elementWithActions-linkout'}>
 				<a
-					href={`${explorer}/tx/${txHash}`}
+					onClick={(e): void => e.stopPropagation()}
+					href={`${explorer}/address/${txHash}`}
 					target={'_blank'}
+					className={'cursor-alias'}
 					rel={'noreferrer'}>
 					<span className={'sr-only'}>{'Link to explorer'}</span>
-					<IconLinkOut className={'w-4 h-4 transition-colors text-primary hover:text-primary-variant'} />
+					<IconLinkOut className={'yearn--elementWithActions-icon'} />
 				</a>
 			</button>
 		</span>
