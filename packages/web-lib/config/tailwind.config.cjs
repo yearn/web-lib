4f036c50252f65e4e224a2c3344054d02f990ac9
Majorfi <major-tom.eth@pm.me>
2022-06-15 12:00:38 +0200

    patch: 0.8.13

--- packages/web-lib/config/tailwind.config.cjs
+++ packages/web-lib/config/tailwind.config.cjs
@@ -7,11 +7,11 @@ module.exports = {
 	content: [
 		'./pages/**/*.{js,jsx,ts,tsx}',
 		'./components/**/*.{js,jsx,ts,tsx}',
-		join(__dirname, '..', 'layouts', '**', '*.{js,jsx,ts,tsx}'),
-		join(__dirname, '..', 'components', '**', '*.{js,jsx,ts,tsx}'),
-		join(__dirname, '..', 'contexts', '**', '*.{js,jsx,ts,tsx}'),
-		join(__dirname, '..', 'icons', '**', '*.{js,jsx,ts,tsx}'),
-		join(__dirname, '..', 'utils', '**', '*.{js,jsx,ts,tsx}')
+		join(__dirname, '..', 'layouts', '*.{js,jsx,ts,tsx}'),
+		join(__dirname, '..', 'components', '*.{js,jsx,ts,tsx}'),
+		join(__dirname, '..', 'contexts', '*.{js,jsx,ts,tsx}'),
+		join(__dirname, '..', 'icons', '*.{js,jsx,ts,tsx}'),
+		join(__dirname, '..', 'utils', '*.{js,jsx,ts,tsx}')
 	],
 	plugins: []
 };
