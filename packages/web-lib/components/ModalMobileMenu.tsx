1388c2c92dc339233ebb3354091d27e2c43924c8
Majorfi <major-tom.eth@pm.me>
2022-07-26 19:12:46 +0200

    patch: 0.11.21

--- packages/web-lib/components/ModalMobileMenu.tsx
+++ packages/web-lib/components/ModalMobileMenu.tsx
@@ -156,8 +156,8 @@ function	ModalMobileMenu({
 								className={'yearn--select-no-arrow yearn--select-reset !pr-6 text-sm'}>
 								{optionsForSelect}
 							</select>
-							<div className={'absolute right-2'}>
-								<svg xmlns={'http://www.w3.org/2000/svg'} viewBox={'0 0 448 512'} className={'ml-1 h-3 w-3'}>
+							<div className={'yearn--modalMobileMenu-chevron'}>
+								<svg xmlns={'http://www.w3.org/2000/svg'} viewBox={'0 0 448 512'}>
 									<path d={'M224 416c-8.188 0-16.38-3.125-22.62-9.375l-192-192c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L224 338.8l169.4-169.4c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25l-192 192C240.4 412.9 232.2 416 224 416z'} fill={'currentcolor'} />
 								</svg>
 							</div>
