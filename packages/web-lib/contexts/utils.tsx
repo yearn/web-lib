6b66f7630b0519e6b9583682336a9786052cb948
Majorfi <major-tom.eth@pm.me>
2022-09-12 19:12:15 +0200

    patch: 0.13.17

--- packages/web-lib/contexts/utils.tsx
+++ packages/web-lib/contexts/utils.tsx
@@ -1,21 +1,21 @@
 export function deepMerge(target: any, source: any): unknown {
-	const isObject = (obj: any) => obj && typeof obj === 'object';
+	const isObject = (obj: any): boolean => obj && typeof obj === 'object';
 
 	if (!isObject(target) || !isObject(source)) {
 		return target;
 	}
 
-	Object.keys(target).forEach(key => {
+	Object.keys(target).forEach((key: string | number): void => {
 		const targetValue = target[key];
 		target[key] = targetValue;
 	});
 
-	Object.keys(source).forEach(key => {
+	Object.keys(source).forEach((key: string | number): void => {
 		const targetValue = target[key];
 		const sourceValue = source[key];
 
 		if (Array.isArray(targetValue) && Array.isArray(sourceValue)) {
-			target[key] = targetValue.concat(sourceValue);
+			target[key] = sourceValue; //no concat, replace
 		} else if (isObject(targetValue) && isObject(sourceValue)) {
 			target[key] = deepMerge(Object.assign({}, targetValue), sourceValue);
 		} else {
