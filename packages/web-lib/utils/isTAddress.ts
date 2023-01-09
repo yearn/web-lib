0715583430bc8985ee6828e4b34547206b0784ef
Karelian Pie <devkarelianpie@gmail.com>
2023-01-09 16:29:22 +0200

    feat: Add `isTAddress` type guard

---
+++ packages/web-lib/utils/isTAddress.ts
@@ -0,0 +1,6 @@
+import { TAddress } from "./address";
+
+export function isTAddress(address?: string | null): address is TAddress {
+	const regex = /^0x([0-9a-f][0-9a-f])*$/i;
+	return !!address && regex.test(address);
+}
