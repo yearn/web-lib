61a9627eb04b09be403855009d2c5188eb747d0b
Majorfi <major-tom.eth@pm.me>
2023-01-16 12:45:55 +0100

    bump

--- packages/web-lib/hooks/useDebounce.tsx
+++ packages/web-lib/hooks/useDebounce.tsx
@@ -21,6 +21,6 @@ export function useDebounce(value: unknown, delay: number): unknown {
 	return debouncedValue;
 }
 
-export {useDebouncedCallback} from '@react-hookz/web/esm/useDebouncedCallback';
-export {useDebouncedEffect} from '@react-hookz/web/esm/useDebouncedEffect';
-export {useDebouncedState} from '@react-hookz/web/esm/useDebouncedState';
+export {useDebouncedCallback} from '@react-hookz/web/cjs/useDebouncedCallback';
+export {useDebouncedEffect} from '@react-hookz/web/cjs/useDebouncedEffect';
+export {useDebouncedState} from '@react-hookz/web/cjs/useDebouncedState';
