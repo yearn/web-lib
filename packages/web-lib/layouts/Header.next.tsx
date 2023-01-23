535bde9b2c2a71da19b4b8bd963fb76aca15f9fc
Majorfi <major-tom.eth@pm.me>
2023-01-23 11:58:48 +0100

    bump

--- packages/web-lib/layouts/Header.next.tsx
+++ packages/web-lib/layouts/Header.next.tsx
@@ -107,21 +107,19 @@ function	NetworkSelector({supportedChainID}: {supportedChainID: number[]}): Reac
 							leave={'transition duration-75 ease-out'}
 							leaveFrom={'transform scale-100 opacity-100'}
 							leaveTo={'transform scale-95 opacity-0'}>
-							<div className={'yearn--shadow'}>
-								<Listbox.Options className={'yearn--listbox-menu -ml-1 bg-neutral-0'}>
-									{supportedNetworks.map((network): ReactElement => (
-										<Listbox.Option key={network.value} value={network}>
-											{({active}): ReactElement => (
-												<div
-													data-active={active}
-													className={'yearn--listbox-menu-item text-sm'}>
-													{network?.label || 'Ethereum'}
-												</div>
-											)}
-										</Listbox.Option>
-									))}
-								</Listbox.Options>
-							</div>
+							<Listbox.Options className={'yearn--listbox-menu yearn--shadow -ml-1 bg-neutral-0'}>
+								{supportedNetworks.map((network): ReactElement => (
+									<Listbox.Option key={network.value} value={network}>
+										{({active}): ReactElement => (
+											<div
+												data-active={active}
+												className={'yearn--listbox-menu-item text-sm'}>
+												{network?.label || 'Ethereum'}
+											</div>
+										)}
+									</Listbox.Option>
+								))}
+							</Listbox.Options>
 						</Transition>
 					</>
 				)}
