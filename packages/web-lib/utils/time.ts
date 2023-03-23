b3958696bac1e9502a562d6c8ee0e8ba5836e951
Karelian Pie <devkarelianpie@gmail.com>
2023-03-23 09:24:24 +0200

    refactor: Time utils and add tests

--- packages/web-lib/utils/time.ts
+++ packages/web-lib/utils/time.ts
@@ -8,14 +8,26 @@ export const WEEK: TMilliseconds = DAY * 7;
 export const YEAR: TMilliseconds = DAY * 365;
 
 export function toTime(time: number | string | undefined): TMilliseconds {
-	return Number(time || 0);
+	if (time === undefined) {
+		return 0;
+	}
+
+	const parsedTime = Number(time);
+
+	if (isNaN(parsedTime)) {
+		throw new Error(`Invalid input: Cannot convert "${time}" to a number.`);
+	}
+
+	return parsedTime;
 }
+
 export function toMilliseconds(time?: TSeconds): TMilliseconds {
 	if (!time) {
 		return 0;
 	}
 	return toTime(time) * SECOND;
 }
+
 export function toSeconds(time?: TMilliseconds, floor = true): TSeconds {
 	if (!time) {
 		return 0;
@@ -23,6 +35,7 @@ export function toSeconds(time?: TMilliseconds, floor = true): TSeconds {
 	const seconds = toTime(time) / SECOND;
 	return floor ? Math.floor(seconds) : seconds;
 }
+
 export function toWeeks(time?: TMilliseconds, floor = true): TWeeks {
 	if (!time) {
 		return 0;
@@ -30,12 +43,14 @@ export function toWeeks(time?: TMilliseconds, floor = true): TWeeks {
 	const weeks = toTime(time) / WEEK;
 	return floor ? Math.floor(weeks) : weeks;
 }
+
 export function fromWeeks(time?: TWeeks): TMilliseconds {
 	if (!time) {
 		return 0;
 	}
 	return toTime(time) * WEEK;
 }
+
 export function getTimeUntil(time?: TMilliseconds): TMilliseconds {
 	if (!time) {
 		return 0;
@@ -43,10 +58,10 @@ export function getTimeUntil(time?: TMilliseconds): TMilliseconds {
 	const duration = toTime(time) - Date.now();
 	return duration < 0 ? 0 : duration;
 }
+
 export function roundToWeek(time?: TMilliseconds): TMilliseconds {
 	if (!time) {
 		return 0;
 	}
 	return Math.floor(toTime(time) / WEEK) * WEEK;
 }
-
