89dd484d4a6b03b32783ba9d62383030467394a0
Major <major@ycorpo.com>
2023-05-13 11:02:27 +0200

    feat: bump

--- packages/web-lib/contexts/WithYearn.tsx
+++ packages/web-lib/contexts/WithYearn.tsx
@@ -6,8 +6,13 @@ import	{Web3ContextApp}						from	'@yearn-finance/web-lib/contexts/useWeb3';
 import	{connectors}							from	'@yearn-finance/web-lib/utils/web3/connectors';
 
 import type {ReactElement} from 'react';
+import type {CoinbaseWallet} from '@web3-react/coinbase-wallet';
 import type {Web3ReactHooks} from '@web3-react/core';
-import	type {Connector}						from	'@web3-react/types';
+import type {EIP1193} from '@web3-react/eip1193';
+import type {GnosisSafe} from '@web3-react/gnosis-safe';
+import type {MetaMask} from '@web3-react/metamask';
+import type {WalletConnect} from '@web3-react/walletconnect';
+import type {Frame} from '../utils/web3/connectors.frame';
 import type {TSettingsBase, TSettingsOptions, TUIOptions, TWeb3Options} from './types';
 
 function	WithYearn({children, options}: {
@@ -19,7 +24,8 @@ function	WithYearn({children, options}: {
 		baseSettings?: Partial<TSettingsBase>,
 	}
 }): ReactElement {
-	const web3Connectors: [Connector, Web3ReactHooks][] = [
+	type TConnectors = MetaMask | WalletConnect | CoinbaseWallet | GnosisSafe | Frame | (EIP1193 & any);
+	const web3Connectors: [TConnectors, Web3ReactHooks][] = [
 		[connectors.frame.connector, connectors.frame.hooks],
 		[connectors.metamask.connector, connectors.metamask.hooks],
 		[connectors.walletConnect.connector, connectors.walletConnect.hooks],
