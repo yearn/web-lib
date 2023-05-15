c5b28f0c1a6a84a2edb7d4fd1256382d8ea2b2df
Majorfi <major-tom.eth@pm.me>
2023-05-15 18:33:13 +0200

    feat: useAutoConnect
    bump
    
    fix: revert change
    
    fix: revert change
    
    fix: revert change

--- packages/web-lib/hooks/useAutoConnect.ts
+++ packages/web-lib/hooks/useAutoConnect.ts
@@ -4,13 +4,11 @@ import {useConnect} from 'wagmi';
 const AUTOCONNECTED_CONNECTOR_IDS = ['safe'];
 
 function useAutoConnect(): void {
-	const {connect, connectors, error, isError, isIdle, isLoading, isSuccess} = useConnect();
+	const {connect, connectors} = useConnect();
 
-	console.log({error, isError, isIdle, isLoading, isSuccess, connectors});
 	useEffect((): void => {
 		AUTOCONNECTED_CONNECTOR_IDS.forEach((connector): void => {
 			const connectorInstance = connectors.find((c): boolean => c.id === connector && c.ready);
-			console.log(connectorInstance, connector);
 
 			if (connectorInstance) {
 				connect({connector: connectorInstance, chainId: 1});
