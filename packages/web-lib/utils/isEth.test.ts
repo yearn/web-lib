a1a0b60e7ae9f9ec978490deb0a1a03c43dc68eb
Karelian Pie <devkarelianpie@gmail.com>
2023-06-06 14:09:06 +0300

    feat: Add `isEth` function

---
+++ packages/web-lib/utils/isEth.test.ts
@@ -0,0 +1,28 @@
+import {ETH_TOKEN_ADDRESS} from '@yearn-finance/web-lib/utils/constants';
+import {isEth} from '@yearn-finance/web-lib/utils/isEth';
+
+describe('isEth function', (): void => {
+	it('returns true for ETH_TOKEN_ADDRESS', (): void => {
+		expect(isEth(ETH_TOKEN_ADDRESS)).toBe(true);
+	});
+
+	it('returns false for null', (): void => {
+		expect(isEth(null)).toBe(false);
+	});
+
+	it('returns false for undefined', (): void => {
+		expect(isEth(undefined)).toBe(false);
+	});
+
+	it('returns false for random string', (): void => {
+		expect(isEth('random string')).toBe(false);
+	});
+
+	it('returns false for random ethereum address', (): void => {
+		expect(isEth('0x627306090abab3a6e1400e9345bc60c78a8bef57')).toBe(false);
+	});
+
+	it('returns true for the string form of ETH_TOKEN_ADDRESS', (): void => {
+		expect(isEth('0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee')).toBe(true);
+	});
+});
