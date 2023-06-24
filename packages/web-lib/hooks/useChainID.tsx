26af1f4c91e09a399fad0e11d3e9bc95b6c7b81d
Major <major@ycorpo.com>
2023-06-24 10:32:41 +0200

    feat: bump

--- packages/web-lib/hooks/useChainID.tsx
+++ packages/web-lib/hooks/useChainID.tsx
@@ -7,6 +7,13 @@ export type TUseChainIDRes = {
 	safeChainID: number;
 }
 
+export const toSafeChainID = (chainID: number, fallback: number): number => {
+	if ([1337, 31337].includes(chainID)) {
+		return fallback;
+	}
+	return chainID;
+};
+
 /* 🔵 - Yearn Finance ******************************************************
 ** This hook can be used to grab the current injected wallet provider.
 ** It will return the name and icon of the wallet provider.
@@ -15,7 +22,7 @@ export function useChainID(defaultChainID?: number): TUseChainIDRes {
 	const {chainID, onSwitchChain} = useWeb3();
 	const safeChainID = useMemo((): number => {
 		const fallbackChainID = defaultChainID || 1;
-		return [1337, 31337].includes(chainID) ? fallbackChainID : chainID || fallbackChainID;
+		return toSafeChainID(chainID, fallbackChainID);
 	}, [chainID, defaultChainID]);
 
 	return ({
