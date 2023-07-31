2ed2b75b48cc6662601dd8b6e3a87a3cc2d435b0
Majorfi <major-tom.eth@pm.me>
2023-07-31 17:09:03 +0200

    bump

--- packages/web-lib/utils/performBatchedUpdates.ts
+++
@@ -1,9 +0,0 @@
-import {unstable_batchedUpdates} from 'react-dom';
-
-function performBatchedUpdates(callback: () => void): void {
-	unstable_batchedUpdates((): void => {
-		callback();
-	});
-}
-
-export default performBatchedUpdates;
--- packages/web-lib/utils/schemas/addressSchema.ts
+++
@@ -1,9 +0,0 @@
-import {z} from 'zod';
-
-import type {TAddress} from '@yearn-finance/web-lib/types';
-
-export const ADDRESS_REGEX = new RegExp(/^0x[0-9a-f]{40}$/i);
-
-export const addressSchema = z.custom<TAddress>((val): boolean => {
-	return ADDRESS_REGEX.test(val as TAddress);
-});
