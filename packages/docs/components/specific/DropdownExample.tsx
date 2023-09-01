99cb36e6297a0ca7d72c6801b3436d4df8465158
0xBasically <0xBasic@yearn.finance>
2023-09-01 10:38:44 -0400

    add: rebase

---
+++ packages/docs/components/specific/DropdownExample.tsx
@@ -0,0 +1,33 @@
+import React, {useState} from 'react';
+import {CodeExample} from 'components/CodeExample';
+import {Dropdown} from '@yearn-finance/web-lib/components/Dropdown';
+import IconNetworkArbitrum from '@yearn-finance/web-lib/icons/IconNetworkArbitrum';
+import IconNetworkEthereum from '@yearn-finance/web-lib/icons/IconNetworkEthereum';
+import IconNetworkFantom from '@yearn-finance/web-lib/icons/IconNetworkFantom';
+import IconNetworkOptimism from '@yearn-finance/web-lib/icons/IconNetworkOptimism';
+import IconNetworkBase from '@yearn-finance/web-lib/icons/IconNetworkBase';
+
+function DropdownExample(): React.ReactElement {
+	const	options = [
+		{icon: <IconNetworkEthereum />, label: 'Ethereum', value: 1},
+		{icon: <IconNetworkOptimism />, label: 'Optimism', value: 10},
+		{icon: <IconNetworkFantom />, label: 'Fantom', value: 250},
+		{icon: <IconNetworkBase />, label: 'Base', value: 8453},
+		{icon: <IconNetworkArbitrum />, label: 'Arbitrum', value: 42161},
+		{label: 'No Icon', value: 123}
+	];
+	const	[selectedOption, set_selectedOption] = useState(options[0]);
+
+	return (
+		<CodeExample>
+			<Dropdown
+				defaultOption={options[0]}
+				options={options}
+				selected={selectedOption}
+				onSelect={(option: any): void => set_selectedOption(option)} />
+		</CodeExample>
+	);
+}
+
+export {DropdownExample};
+export default DropdownExample;
